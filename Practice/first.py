8eed6a84a788cb8c5af856355f1f6f7b0ceaf9f6
aveshkhan2266 <kavesh833@gmail.com>
2025-12-03 17:13:17 +0530

    practice-code

---
+++ Practice/first.py
@@ -0,0 +1,20 @@
+studentlist=[]
+student={}
+student["id"]=int(input("Enter student id: "))
+student["name"]=(input("Enter student name: "))
+student["email"]=(input("Enter student email: "))
+studentlist.append(student)
+
+student={}
+student["id"]=int(input("Enter student id: "))
+student["name"]=(input("Enter student name: "))
+student["email"]=(input("Enter student email: "))
+studentlist.append(student)
+
+student={}
+student["id"]=int(input("Enter student id: "))
+student["name"]=(input("Enter student name: "))
+student["email"]=(input("Enter student email: "))
+studentlist.append(student)
+
+print(studentlist)
